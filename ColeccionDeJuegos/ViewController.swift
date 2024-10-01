f43062d2e61e9543d79c076e1b174c9562f81635
SuicoRamiro <ramiro.suico@tecsup.edu.pe>
2024-10-01 04:55:48 -0500

    MisPrimerCommit

--- ColeccionDeJuegos/ViewController.swift
+++ ColeccionDeJuegos/ViewController.swift
@@ -1,17 +1,36 @@
-//
-//  ViewController.swift
-//  ColeccionDeJuegos
-//
-//  Created by Ramiro on 1/10/24.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    var juegos : [Juego] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        do {
+            try juegos = context.fetch(Juego.fetchRequest())
+            tableView.reloadData()
+        } catch {
+            
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return juegos.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let juego = juegos[indexPath.row]
+        cell.textLabel?.text = juego.titulo
+        cell.imageView?.image = UIImage(data: juego.imagen!)
+        return cell
     }
 
 
